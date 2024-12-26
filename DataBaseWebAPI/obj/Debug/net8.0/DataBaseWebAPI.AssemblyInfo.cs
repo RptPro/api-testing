f9ada117eeb3556df4ec04095352fd2f719c5fbe
RptPro <rptprogrammers@gmail.com>
2024-12-26 16:45:09 +0200

    Template Remove

--- DataBaseWebAPI/obj/Debug/net8.0/DataBaseWebAPI.AssemblyInfo.cs
+++ DataBaseWebAPI/obj/Debug/net8.0/DataBaseWebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataBaseWebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a885893420c9049c6b7c15d0cdc559108502ffe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f41b931921c0a1152f875609642214c7f3619c25")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataBaseWebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataBaseWebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
